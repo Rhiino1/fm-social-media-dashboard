37834c84dc330fed8a4ecd1eed005bd477835101
Cesar SC <csolanoc@unal.edu.co>
2023-09-24 18:18:17 -0500

    Added active state

--- src/components/BigCard.tsx
+++ src/components/BigCard.tsx
@@ -16,7 +16,7 @@ function BigCard(props: {
       <section
         className={`w-[326px] h-[216px] rounded-md rounded-b-md  ${social}`}
       >
-        <article className="w-[inherit] h-[212px] rounded-b-md grid grid-flow-row grid-rows-3 items-center justify-center bg-[#F0F2FA]  mt-[4px]">
+        <article className="w-[inherit] h-[212px] rounded-b-md grid grid-flow-row grid-rows-3 items-center justify-center hover:bg-[#F0F2FA] bg-[#F5F7FF]  mt-[4px]">
           <div className="flex items-center justify-center gap-2">
             <img src={img} alt="Icon" />
             <p className="font-inter text-[14px] text-[#63687E] font-bold">
@@ -33,7 +33,11 @@ function BigCard(props: {
           </div>
           <div className="flex items-center justify-center gap-2">
             <img src={isUp ? upImg : downImg} alt="Up/Down icon" />
-            <p className={`font-inter text-[14px] ${isUp ? 'text-[#1DB489]' : 'text-[#DC414C]'} font-bold`}>
+            <p
+              className={`font-inter text-[14px] ${
+                isUp ? "text-[#1DB489]" : "text-[#DC414C]"
+              } font-bold`}
+            >
               {todayFollows} Today
             </p>
           </div>
