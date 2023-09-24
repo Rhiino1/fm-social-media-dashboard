8f100ead2221551bb87f5df681eac5b0ee6fb150
Rhino <36159485+Rhiino1@users.noreply.github.com>
2023-09-24 12:54:56 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/layouts/Footer";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col h-screen justify-between bg-[#d5e1ef]">
+        <h1 className="text-5xl font-bold text-red-300">Tailwindcss working</h1>
+        <Footer></Footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
